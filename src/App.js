9a1437bc584afa3af243becff50c98bc8bd8f877
raviteja-08 <128289383+raviteja-08@users.noreply.github.com>
2023-07-18 23:03:25 +0530

    updated-ui , logo and fixed some responsive issues

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import {Box} from '@mui/material';
 import './index.css'
-import {Feed,Navbar,VideoDetail,SearchFeed,ChannelDetail} from "./components"
+import {Feed,Navbar,VideoDetail,SearchFeed,ChannelDetail,PlaylistPlaying} from "./components"
 import LoadingBar from 'react-top-loading-bar';
 
 
@@ -26,6 +26,8 @@ const App = () => {
             {/* <Route path="/feed/:category" exact element={<Feed/>} setLoadingProgress={setLoadingProgress}/> */}
             <Route path ='/video/:id' element={<VideoDetail setLoadingProgress={setLoadingProgress}/>} />
             <Route path='/channel/:id' element ={<ChannelDetail setLoadingProgress={setLoadingProgress}/>}/>
+            {/* <Route path='/playlist/:id/:position' element={<PlaylistPlaying/>}/> */}
+            <Route path='/playlist/:id/:position' element={<PlaylistPlaying/>}/>
             <Route path='/search/:searchTerm' element={<SearchFeed setLoadingProgress={setLoadingProgress}/>} />
 
             
